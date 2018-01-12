07d978309c67a0e46e4a9d65e7c63d73414cf7da
Lisa Lee <snickers495@gmail.com>
2018-01-12 19:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function upperCaseCustomerName() {
 }
 
 function setBestCustomer(){
-  var bestCustomer = "not bob";
+  bestCustomer = "not bob";
   return bestCustomer;
 }
 function overwriteBestCustomer(){
