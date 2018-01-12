2944f6601f05e4021340b820135c7ed545b64ef1
Lisa Lee <snickers495@gmail.com>
2018-01-12 19:47:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 var customerName = 'bob'
 
 function upperCaseCustomerName(customerName) {
-  return customerName[0].toUpperCase() + customerName.slice(1)
+  return customerName[0].toUpperCase() + customerName.slice(1);
 }
